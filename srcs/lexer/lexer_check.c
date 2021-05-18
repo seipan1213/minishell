3f10db456fbd588df65cce00fe10f72dd010d63d
seipan1213 <seiju1213@gmail.com>
2021-05-18 21:28:09 +0900

    #4 QUOTEの処理変更

--- srcs/lexer/lexer_check.c
+++ srcs/lexer/lexer_check.c
@@ -12,26 +12,15 @@
 
 #include "../../includes/lexer.h"
 
-void	check_qoute(int *flag, int type)
-{
-	if (type == SQUOTE)
-		*flag ^= 1;
-	else if (type == DQUOTE)
-		*flag ^= 2;
-}
-
 int		check_syntax(t_token *t)
 {
 	t_ttype	pre_type;
-	int		quote_flag;
 
 	pre_type = -1;
-	quote_flag = 0;
-	while (t)
+	while (t->next)
 	{
 		while (t->type == SPACE && t->next)
 			t = t->next;
-		check_qoute(&quote_flag, t->type);
 		if ((is_metatype(t->type) && is_rdcttype(pre_type))
 			|| (is_metatype(pre_type) && is_rdcttype(t->type))
 			|| (is_metatype(t->type) && is_metatype(pre_type))
@@ -40,11 +29,10 @@ int		check_syntax(t_token *t)
 			|| t->type == DSCOLON)
 			return (put_error(SYNTAXERR, -1));
 		pre_type = t->type;
-		if (t)
+		if (t->next)
 			t = t->next;
 	}
-	if (quote_flag != 0 || (t &&
-		(is_rdcttype(t->type) || t->type == PIPE || t->type == ESC)))
+	if (t && (is_rdcttype(t->type) || t->type == PIPE || t->type == ESC))
 		return (put_error(SYNTAXERR, -1));
 	return (0);
 }
