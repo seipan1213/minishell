7f87299e93931aefd4f99b4be1dd1bacab51dc56
seipan1213 <seiju1213@gmail.com>
2021-05-19 20:46:57 +0900

    #4 lexer 間のスペース除去

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -12,39 +12,60 @@
 
 #include "../../includes/lexer.h"
 
-void	token_join(t_token *t)
+void	lexer_str(t_token *t)
 {
 	t_token	*tmp;
-	char	*str;
+	char		*str;
 
-	str = ft_strjoin(t->str, t->next->str);
-	free(t->str);
-	t->str = str;
-	tmp = t->next;
-	t->next = t->next->next;
-	free(tmp->str);
-	free(tmp);
-}
-
-void	lexer_tokens(t_token *t)
-{
 	while(t && t->next)
 	{
 		while (t->next && t->type == t->next->type && t->type == STR)
-			token_join(t);
+		{
+			str = ft_strjoin(t->str, t->next->str);
+			free(t->str);
+			t->str = str;
+			tmp = t->next;
+			t->next = t->next->next;
+			free(tmp->str);
+			free(tmp);
+		}
 		t = t->next;
 	}
 }
 
-int		main()
+void	lexer_space(t_token *t)
 {
-	char *line;
-	t_token *t;
+	t_token	*tmp;
 
-	ft_putstr_fd(MINISHELL, STDERR_FILENO);
-	get_next_line(0, &line);
-	t = tokenise(line);
-	check_tokens(t);
-	lexer_tokens(t);
-	print_tokens(t);
+	while(t && t->next)
+	{
+		if (t->next->type == SPACE)
+		{
+			tmp = t->next;
+			t->next = tmp->next;
+			free(tmp->str);
+			free(tmp);
+		}
+		else
+			t = t->next;
+	}
 }
+
+void	lexer_tokens(t_token *t)
+{
+	lexer_str(t);
+	lexer_space(t);
+}
+
+//int		main()
+//{
+//	char *line;
+//	t_token *t;
+//
+//	ft_putstr_fd(MINISHELL, STDERR_FILENO);
+//	get_next_line(0, &line);
+//	t = tokenise(line);
+//	check_tokens(t);
+//	lexer_tokens(t);
+//	print_tokens(t);
+//}
