bb410f97c60b0119ee1d64296f2db9b54c58e49a
seipan1213 <seiju1213@gmail.com>
2021-06-13 21:22:21 +0900

    #69 仮実装　バグあり

--- srcs/builtin/built_echo.c
+++ srcs/builtin/built_echo.c
@@ -3,17 +3,25 @@
 int	built_echo(char **argv)
 {
 	int	i;
+	int	j;
 	int	flag;
 
 	i = 1;
 	flag = 0;
-	while (argv[i] && !ft_strncmp(argv[i], "-n", 3) && ++i)
-		flag |= ECHO_OP_N;
+	while (argv[i] && argv[i][0] == '-')
+	{
+		j = 1;
+		while (argv[i][j] == 'n')
+			j++;
+		if (argv[i][j] == ' ' && ++i)
+			flag |= ECHO_OP_N;
+		else
+			break ;
+	}
 	while (argv[i] != NULL)
 	{
 		ft_putstr_fd(argv[i], STDOUT_FILENO);
-		i++;
-		if (argv[i] != NULL)
+		if (argv[i++] != NULL)
 			ft_putstr_fd(" ", STDOUT_FILENO);
 	}
 	if (!(flag & ECHO_OP_N))
