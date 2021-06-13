a5a405d819a99514a0d135ed3340c28317de65fc
seipan1213 <seiju1213@gmail.com>
2021-06-13 17:58:10 +0900

    signal周り修正

--- srcs/builtin/built_exit.c
+++ srcs/builtin/built_exit.c
@@ -8,7 +8,7 @@ int	built_exit(char **argv)
 	i = 1;
 	if (g_data.quit == 1)
 		ft_putendl_fd("exit", STDOUT_FILENO);
-	if (argv[i] == NULL)
+	if (argv == NULL || argv[i] == NULL)
 		exit(g_data.states);
 	num = ft_atoi(argv[i]);
 	if (is_strdigit(argv[i]) && !argv[i + 1])
