c54c78e681bb26daac1030b0d2d58e3638909154
kotatabe <qwerty789kota@gmail.com>
2021-06-15 19:19:30 +0900

    #47 複数リダイレクトのexec(パイプしてる場合のみ)

--- srcs/exec/exec_simple_builtin.c
+++ srcs/exec/exec_simple_builtin.c
@@ -1,60 +1,87 @@
 #include "../../includes/exec.h"
 
-void	backup_cur_fd(t_command *cmd)
+void	backup_cur_fd(t_redirect *rd)
 {
-	if (!cmd->rd)
+	t_redirect	*rd_now;
+
+	if (!rd)
 		return ;
-	if (cmd->rd->fd_file != NOT_SPECIFIED)
+	rd_now = rd;
+	while (rd_now)
 	{
-		cmd->rd->fd_backup = dup(cmd->rd->fd_file);
-		return ;
-	}
-	if (cmd->rd->type == RD_INPUT)
-	{
-		cmd->rd->fd_backup = dup(STDIN_FILENO);
-	}
-	else if (cmd->rd->type == RD_OUTPUT || cmd->rd->type == RD_APPEND_OUTPUT)
-	{
-		if (cmd->rd->fd_file == STDERR_FILENO)
-			cmd->rd->fd_backup = dup(STDERR_FILENO);
-		else
-			cmd->rd->fd_backup = dup(STDOUT_FILENO);
+		if (rd_now->fd_file != NOT_SPECIFIED)
+		{
+			rd_now->fd_backup = dup(rd_now->fd_file);
+			return ;
+		}
+		if (rd_now->type == RD_INPUT)
+		{
+			rd_now->fd_backup = dup(STDIN_FILENO);
+		}
+		else if (rd_now->type == RD_OUTPUT || rd_now->type == RD_APPEND_OUTPUT)
+		{
+			if (rd_now->fd_file == STDERR_FILENO)
+				rd_now->fd_backup = dup(STDERR_FILENO);
+			else
+				rd_now->fd_backup = dup(STDOUT_FILENO);
+		}
+		rd_now = rd_now->next;
 	}
 }
 
-void	recover_fd(t_command *cmd)
+void	recover_fd(t_redirect *rd)
 {
-	if (!cmd->rd)
+	t_redirect	*rd_now;
+
+	if (!rd)
 		return ;
-	if (cmd->rd->type == RD_INPUT)
+	rd_now = rd;
+	while (rd_now)
 	{
-		dup2(cmd->rd->fd_backup, STDIN_FILENO);
-		close(cmd->rd->fd_backup);
-	}
-	else if (cmd->rd->type == RD_OUTPUT || cmd->rd->type == RD_APPEND_OUTPUT)
-	{
-		dup2(cmd->rd->fd_backup, STDOUT_FILENO);
-		close(cmd->rd->fd_backup);
+		if (rd_now->fd_file != NOT_SPECIFIED)
+		{
+			dup_fd(rd_now->fd_backup, rd_now->fd_file);
+			return ;
+		}
+		if (rd_now->type == RD_INPUT)
+		{
+			dup_fd(rd_now->fd_backup, STDIN_FILENO);
+		}
+		else if (rd_now->type == RD_OUTPUT || rd_now->type == RD_APPEND_OUTPUT)
+		{
+			dup_fd(rd_now->fd_backup, STDOUT_FILENO);
+		}
+		rd_now = rd_now->next;
 	}
 }
 
-void	close_rd_fd(t_command *cmd)
+void	close_rd_fd(t_redirect *rd)
 {
-	if (!cmd->rd)
+	if (!rd)
 		return ;
-	if (cmd->rd->fd_file != NOT_SPECIFIED)
-		close(cmd->rd->fd_file);
+	if (rd->fd_file != NOT_SPECIFIED)
+		close(rd->fd_file);
 }
+
+	void		print_rd(t_redirect *rd)
+	{
+		while (rd)
+		{
+			printf("[%s]\n", rd->filename->str);
+			rd = rd->next;
+			if (!rd)
+				printf("p: %p\n", rd);
+		}
+	}
 
 int		exec_simple_buildin(t_command *cmd, char **args)
 {
-	int	ret;
+	int		ret;
 
-	backup_cur_fd(cmd);
-	get_rd_fd(cmd);
-	change_rd_fd(cmd);
+	backup_cur_fd(cmd->rd);
+	get_rd_fd(cmd->rd);
+	change_rd_fd(cmd->rd);
 	ret = exec_builtin(args);
-	close_rd_fd(cmd);
 	// if (errno)
 	// {
 	// 	ft_putstr_fd(args[0], STDERR_FILENO);
@@ -63,6 +90,7 @@ int		exec_simple_buildin(t_command *cmd, char **args)
 	// 	ft_putstr_fd("\n", STDERR_FILENO);
 	// }
 	// errno = 0;
-	recover_fd(cmd);
+	recover_fd(cmd->rd);
+	close_rd_fd(cmd->rd);
 	return (ret);
 }
