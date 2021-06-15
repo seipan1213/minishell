53b1d3221a5198f6961629a7a07974572e4e96f0
seipan1213 <seiju1213@gmail.com>
2021-06-15 22:11:49 +0900

    #76 RRDもセグフォ修正

--- srcs/lexer/token_meta_2.c
+++ srcs/lexer/token_meta_2.c
@@ -46,7 +46,7 @@ void	token_rrdct(t_tdata *d, t_token **t)
 	t_token	*back;
 
 	back = tokens_last(*t);
-	if (is_strdigit(back->str))
+	if (back && is_strdigit(back->str))
 		back->type = RINT;
 	if (!ft_strncmp(d->line + d->i, ">>", 2))
 	{
