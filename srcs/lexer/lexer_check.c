464e7c86b1faf33f7d82b6530234b9a2ce6fbd7b
seipan1213 <seiju1213@gmail.com>
2021-06-15 22:17:54 +0900

    #76 ; だけの場合にsyntax errできてないのを修正

--- srcs/lexer/lexer_check.c
+++ srcs/lexer/lexer_check.c
@@ -32,7 +32,8 @@ int		check_syntax(t_token *t)
 		if (t->next)
 			t = t->next;
 	}
-	if (t && (is_rdcttype(t->type) || t->type == PIPE || t->type == ESCAPE))
+	if (t && (is_rdcttype(t->type) || t->type == PIPE || t->type == ESCAPE ||
+			t->type == SCOLON || t->type == DSCOLON))
 		return (put_error(SYNTAXERR, 0));
 	return (1);
 }
