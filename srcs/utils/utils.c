dad0500906ea09bba42dcea3b5a22b4f4b3c6f83
seipan1213 <seiju1213@gmail.com>
2021-06-16 23:12:27 +0900

    #76 exit 文字列の数値対応

--- srcs/utils/utils.c
+++ srcs/utils/utils.c
@@ -3,13 +3,19 @@
 int		is_strdigit(char *str)
 {
 	int	i;
+	int	flag;
 
 	i = 0;
+	flag = 0;
 	while (str[i])
 	{
-		if (!ft_isdigit(str[i]))
+		if (ft_isdigit(str[i]))
+			flag = 1;
+		if (!ft_isdigit(str[i]) && !ft_isspace(str[i]))
 			return (0);
 		i++;
 	}
-	return (1);
+	if (flag)
+		return (1);
+	return (0);
 }
