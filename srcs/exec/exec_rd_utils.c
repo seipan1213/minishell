4699f58765b4516f96483ad151349ecd7f6d60b6
kotatabe <qwerty789kota@gmail.com>
2021-06-22 20:12:20 +0900

    #88 ヒアドキュメント作業途中

--- srcs/exec/exec_rd_utils.c
+++ srcs/exec/exec_rd_utils.c
@@ -1,6 +1,37 @@
 
 #include "../../includes/exec.h"
 
+void	get_heredoc(t_redirect *rd)
+{
+	char	*line;
+	while (get_next_line(0, &line) < 0)
+	{
+		
+	}
+}
+
+int	open_file(t_redirect *rd)
+{
+	if (rd->type == RD_INPUT)
+		return (open(rd->filename->str, O_RDONLY));
+	else if (rd->type == RD_OUTPUT)
+	{
+		return (open(rd->filename->str, \
+		O_WRONLY | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE));
+	}
+	else if (rd->type == RD_APPEND_OUTPUT)
+	{
+		return (open(rd->filename->str, \
+		O_WRONLY | O_CREAT | O_APPEND, S_IREAD | S_IWRITE));
+	}
+	else if (rd->type == RD_HERE_DOC)
+	{
+		return(open(".tmpfile", \
+		O_RDONLY | O_CREAT | O_TRUNC, S_IREAD | S_IWRITE));
+	}
+	return(-1);
+}
+
 void	get_rd_fd(t_redirect *rd)
 {
 	t_redirect	*rd_now;
@@ -10,20 +41,8 @@ void	get_rd_fd(t_redirect *rd)
 	rd_now = rd;
 	while (rd_now)
 	{
-		if (rd_now->type == RD_INPUT)
-		{
-			rd_now->fd_io = open(rd_now->filename->str, O_RDONLY);
-		}
-		else if (rd_now->type == RD_OUTPUT)
-		{
-			rd_now->fd_io = open(rd_now->filename->str, O_WRONLY | O_CREAT | O_TRUNC, \
-			S_IREAD | S_IWRITE);
-		}
-		else if (rd_now->type == RD_APPEND_OUTPUT)
-		{
-			rd_now->fd_io = open(rd_now->filename->str, O_WRONLY | O_CREAT | O_APPEND, \
-			S_IREAD | S_IWRITE);
-		}
+		if ((rd_now->fd_io = open_file(rd_now)) < 0)
+			return ;
 		rd_now = rd_now->next;
 	}
 }
