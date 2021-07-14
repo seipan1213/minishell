4ed88aa03c37898cbd67580bde611a50d527c774
kotatabe <qwerty789kota@gmail.com>
2021-07-14 21:17:05 +0900

    #126 signal c readline の関数を追加

--- srcs/utils/signal.c
+++ srcs/utils/signal.c
@@ -6,15 +6,24 @@
 /*   By: kotatabe <kotatabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:05:22 by kotatabe          #+#    #+#             */
-/*   Updated: 2021/06/30 16:05:23 by kotatabe         ###   ########.fr       */
+/*   Updated: 2021/07/14 20:45:52 by kotatabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/utils.h"
 
+void	sigint_handler(int i)
+{
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+	i = 0;
+}
+
 void	set_signal(void (*func)(int))
 {
-	if (signal(SIGINT, func) == SIG_ERR)
+	if (signal(SIGINT, sigint_handler) == SIG_ERR)
 	{
 		ft_putstr_fd(strerror(errno), STDERR_FILENO);
 		exit(1);
