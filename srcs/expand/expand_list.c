0276e65cca10bb5536dbccb299fb189608f03c37
seipan1213 <seiju1213@gmail.com>
2021-07-17 20:44:18 +0900

    #120 norm 対応

--- srcs/expand/expand_list.c
+++ srcs/expand/expand_list.c
@@ -6,7 +6,7 @@
 /*   By: sehattor <sehattor@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:02:09 by kotatabe          #+#    #+#             */
-/*   Updated: 2021/07/17 20:29:46 by sehattor         ###   ########.fr       */
+/*   Updated: 2021/07/17 20:43:46 by sehattor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,25 @@ void	addnext_token(t_token **lst, t_token *t)
 	t->next = tmp;
 }
 
+static void	expand_div_util(t_token **lst, t_token **t)
+{
+	t_token	*token;
+
+	(*lst)->str = front_join((*lst)->str, ft_strdup((*t)->str));
+	*t = (*t)->next;
+	if (*t)
+	{
+		token = create_token(STR, ft_strdup(""));
+		if (!token)
+			exit_error(MALLOCERR, NULL, 1);
+		addnext_token(lst, token);
+		*lst = (*lst)->next;
+	}
+}
+
 void	expand_div(t_token **lst, char *str, int flag)
 {
 	t_token	*t;
-	t_token	*token;
 	t_token	*tmp;
 
 	if (!flag)
@@ -41,18 +56,7 @@ void	expand_div(t_token **lst, char *str, int flag)
 	lexer_tokens(t);
 	tmp = t;
 	while (t)
-	{
-		(*lst)->str = front_join((*lst)->str, ft_strdup(t->str));
-		t = t->next;
-		if (t)
-		{
-			token = create_token(STR, ft_strdup(""));
-			if (!token)
-				exit_error(MALLOCERR, NULL, 1);
-			addnext_token(lst, token);
-			*lst = (*lst)->next;
-		}
-	}
+		expand_div_util(lst, &t);
 	if (tmp)
 		free_token(tmp);
 }
